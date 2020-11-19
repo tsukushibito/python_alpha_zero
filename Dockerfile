95d0c89cb52393f5afdb971f46d232df07f838ec
tsukushibito <tsukushibito@gmail.com>
2020-11-19 12:03:47 +0900

    Add PlantUML

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 ﻿FROM python:3
 
 RUN apt-get update
-RUN apt-get  install -y vim less
+RUN apt-get  install -y vim less graphviz default-jre
 RUN pip install --upgrade pip
 RUN pip install --upgrade setuptools
 RUN pip install --upgrade pipenv
--- alpha_zero/alpha_zero/game.py
+++ alpha_zero/alpha_zero/game.py
@@ -1,3 +1,14 @@
-﻿class Game:
+﻿from abc import ABCMeta, abstractclassmethod
+
+
+class Game(metaclass=ABCMeta):
     def __init__(self):
         self.x = 0
+
+    @abstractclassmethod
+    def step(self, action):
+        pass
+
+
+class Action(metaclass=ABCMeta):
+    pass
